dd9829aaa6d14fc6e4b22488275f7f8385cac1c5
thinkwithabs <twithabs@gmail.com>
2022-09-01 10:40:45 +0600

    first commit

---
+++ src/components/Authentication.jsx
@@ -0,0 +1,67 @@
+import React,{useState} from 'react'
+import { collection, addDoc } from "firebase/firestore"; 
+import { doc, setDoc,getDocs,updateDoc  } from "firebase/firestore"; 
+import {app,db} from "../firebaseConfig"
+
+function Authentication() {
+    const [email, setemail] = useState('')
+    const [last, setpass] = useState('')
+    const [roll, setroll] = useState('')
+    const [reg, setreg] = useState('')
+
+    const collectionref=collection(db,'users')
+ 
+    const submitdhandel =()=>{
+      addDoc(collection(db, "users"), {
+        first: email,
+        last: last,
+        roll: roll,
+        reg:reg,
+      }).then(()=>{
+        alert("data add")
+      })
+      .catch((err)=>{
+        alert(err.message)
+      })
+  
+    }
+  
+    const getData=()=>{
+      getDocs(collectionref).then((res)=>{
+        console.log(
+          res.docs.map((item)=>{
+            return {...item.data(),id:item.id}
+          })
+       
+        )
+      })
+       
+      
+    }
+  
+  
+    const updateData= ()=>{
+      const dostoupdate = doc(db,"users","wpQTqb5ZQPBCc2O7biTK")
+      updateDoc(dostoupdate,{
+        first: email,
+        last: last,
+        roll: roll,
+        reg:reg,
+      }).then((res)=>alert("data update")).catch(err=>alert(err.message))
+    }
+  return (
+    <div>
+        <div>
+            
+            <input type="text" name="" value={email} placeholder="Fast Name" onChange={(e)=>setemail(e.target.value)}/>
+            <input type="text" name="" value={last} placeholder="Last Name" onChange={(e)=>setpass(e.target.value)}/>
+            <input type="text" name="" value={roll} placeholder="Roll...." onChange={(e)=>setroll(e.target.value)}/>
+            <input type="text" name="" value={reg} placeholder="Reg..." onChange={(e)=>setreg(e.target.value)}/>
+            <button type="submit" onClick={getData}>submit</button>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Authentication
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBH_92c0-wXC7krROT3enUEOxAJ69SwXxw",
+  authDomain: "fir-frontend-8ca36.firebaseapp.com",
+  projectId: "fir-frontend-8ca36",
+  storageBucket: "fir-frontend-8ca36.appspot.com",
+  messagingSenderId: "328525466639",
+  appId: "1:328525466639:web:ef7153a62cde91f306a77a"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
